f1004244aa6265cf39ad5f41eb8c8c5f4b6f1184
a-shpontak <andrii.shpontak@cowchain.io>
2024-12-15 17:52:32 +0200

    event emitter practice

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,27 +5,24 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { SchedulerModule } from './scheduler/scheduler.module';
 import { CronModule } from './cron/cron.module';
 import { FibonacciModule } from './fibonacci/fibonacci.module';
-import { HttpClientModule } from './http-client/http-client.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { TagsModule } from './tags/tags.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { PaymentsModule } from './payments/payment.module';
 
 @Module({
   imports: [
+    EventEmitterModule.forRoot(),
     CoffeesModule,
     SchedulerModule,
     CronModule,
     FibonacciModule,
-    HttpClientModule.register({
-      baseUrl: 'https://nestjs.com',
-    }),
-    HttpClientModule.register({
-      baseUrl: 'https://nestjs.com',
-    }),
-    HttpClientModule.register({
-      baseUrl: 'https://nestjs.com',
-    }),
+    // HttpClientModule.register({
+    //   baseUrl: 'https://nestjs.com',
+    // }),
     RecipesModule,
     TagsModule,
+    PaymentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
